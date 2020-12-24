b3cbbf579234bec8aa26c3463222b36bdbe4ff2c
Yuehan <yuehan.huang@protonmail.com>
2020-12-23 18:33:19 -0800

    Add posts context

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,31 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Me',
+        'title': 'first post',
+        'content': 'wefkjwae',
+        'date_posted': '12/23/2020'
+    },
+    {
+        'author': 'Me',
+        'title': 'second post',
+        'content': 'wefkweawefaweajwae',
+        'date_posted': '12/24/2020'
+    },
+    {
+        'author': 'Me',
+        'title': 'third post',
+        'content': 'wefkjwawefaweawefe',
+        'date_posted': '12/25/2020'
+    }
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {'posts': posts}
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
